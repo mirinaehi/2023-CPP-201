2c9fdd3c8aa52949095fc079fa1163d7f89ab9d1
Hokkaido <garook@e-mirim.hs.kr>
2023-03-16 10:54:34 +0900

    변수 출력

--- 2023-CPP-201/문법/CPP 기본.cpp
+++ 2023-CPP-201/문법/CPP 기본.cpp
@@ -7,7 +7,9 @@ int main(void)
 	// std::cout : 출력을 위한 객체
 	// << : 출력연산자
 	// std::endl : 개행(줄바꿈) 객체
-	cout << "Hello World" << endl;
+
+	int a = 2023;
+	cout << "Hello World " << a << endl;
 
 	return 0;
 }
