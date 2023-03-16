cd5abb272962ddd68b0c9519152f3d475d04e601
Hokkaido <garook@e-mirim.hs.kr>
2023-03-16 10:15:15 +0900

    namespace

--- 2023-CPP-201/문법/CPP 기본.cpp
+++ 2023-CPP-201/문법/CPP 기본.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
+using namespace std;		// 이름공간
+
 int main(void)
 {
 	// std::cout : 출력을 위한 객체
 	// << : 출력연산자
 	// std::endl : 개행(줄바꿈) 객체
-	std::cout << "Hello World" << std::endl;
+	cout << "Hello World" << endl;
 
 	return 0;
 }
