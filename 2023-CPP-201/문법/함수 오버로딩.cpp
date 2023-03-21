9c522e370ead523a1895100d26310a208e1784e9
Hokkaido <garook@e-mirim.hs.kr>
2023-03-21 10:05:19 +0900

    함수 오버로딩 완료

--- 2023-CPP-201/문법/함수 오버로딩.cpp
+++ 2023-CPP-201/문법/함수 오버로딩.cpp
@@ -1,18 +1,19 @@
 ﻿#include <iostream>
 
 using namespace std;
-
-int sum_int(int a, int b) {
+// 함수 오버로딩을 해서 함수이름을 간결하게 할 수 있다.
+// 함수 오버로딩을 하면 함수이름은 같다 하지만 매개변수가 다르다
+int sum(int a, int b) {
 	return a + b;
 }
 
-float sum_float(float a, float b) {
+float sum(float a, float b) {
 	return a + b;
 }
 
 int main(void)
 {
-	cout << sum_int(1, 2) << endl;
-	cout << sum_float(1.1f, 2.2f) << endl;
+	cout << sum(1, 2) << endl;
+	cout << sum(1.1f, 2.2f) << endl;
 	return 0;
 }
