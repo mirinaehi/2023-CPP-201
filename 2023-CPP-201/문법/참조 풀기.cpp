320aa23e2efab5787c9461502d61bd4c561aec2a
Hokkaido <garook@e-mirim.hs.kr>
2023-03-21 09:54:01 +0900

    참조로 바꾸기 + 함수 오버로딩 하기 전

--- 2023-CPP-201/문법/참조 풀기.cpp
+++ 2023-CPP-201/문법/참조 풀기.cpp
@@ -2,15 +2,15 @@
 
 using namespace std;
 
-void 더하라2를(int* num)
+void 더하라2를(int& num)
 {
-	*num += 2;
+	num += 2;
 }
 
 int main(void)
 {
 	int a = 3;
-	더하라2를(&a);
+	더하라2를(a);
 
 	cout << a << endl;
 	return 0;
