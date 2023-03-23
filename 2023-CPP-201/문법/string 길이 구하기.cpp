2819e6c4ce374e518cc4eeb7da2a1897c253ec7e
Hokkaido <garook@e-mirim.hs.kr>
2023-03-23 10:53:49 +0900

    C++ 스타일 size() 문자열 길이

--- 2023-CPP-201/문법/string 길이 구하기.cpp
+++ 2023-CPP-201/문법/string 길이 구하기.cpp
@@ -1,10 +1,13 @@
-﻿#include <stdio.h>
-#include <string.h>
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
 
 int main(void)
 {
-	char str[30] = "Jiwoo";
-
-	printf("문자열의 길이는 %d \n", strlen(str));
+	string str = "Jiwoo";
+	
+	// str.length()도 가능하나, size()를 더 권장
+	cout << "문자열의 길이는 " << str.size() << endl;
 	return 0;
 }
