840eff02f8f0faa32bdc802dc81464a8aa9e4320
Hokkaido <garook@e-mirim.hs.kr>
2023-03-23 10:42:54 +0900

    C스타일 문자열 길이구하기

---
+++ 2023-CPP-201/문법/string 길이 구하기.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str[30] = "Jiwoo";
+	int size = 0;
+
+	for(int i=0; str[i] !='\0'; i++){
+		size++;
+	}
+
+	printf("문자열의 길이는 %d \n", size);
+	return 0;
+}
