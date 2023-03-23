0d6ffeb2470ba90205b3dab213b570792ca323aa
Hokkaido <garook@e-mirim.hs.kr>
2023-03-23 10:03:32 +0900

    C스타일 문자열 복사(시험에 안나옴)

---
+++ 2023-CPP-201/문법/string 복사.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <string.h>		// 문자열 관련 함수
+
+int main(void)
+{
+	char str[30] = { 0, };
+	char str2[30] = "c_insert";
+
+	for (int i = 0; str2[i] != '\0'; i++) {
+		str[i] = str2[i];
+	}
+
+	printf("%s \n", str);
+
+	return 0;
+}
