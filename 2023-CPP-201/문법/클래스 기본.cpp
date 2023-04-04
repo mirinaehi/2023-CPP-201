e6f3023dfec3a5109134f6c40127657a7e0f75a1
Hokkaido <garook@e-mirim.hs.kr>
2023-04-04 09:00:13 +0900

    생성자란?

--- 2023-CPP-201/문법/클래스 기본.cpp
+++ 2023-CPP-201/문법/클래스 기본.cpp
@@ -20,6 +20,10 @@ public:
 	int sex;
 	string department;
 
+	// 생성자(constructor) : 객체가 생성될 때 호출되는 함수
+	// 생성자를 정의하지 않으면 default로 매개변수가 없는 생성자가 정의된다
+	// Student(){}
+
 	// class는 멤버변수를 가질 수 있다
 	void print(void)
 	{
