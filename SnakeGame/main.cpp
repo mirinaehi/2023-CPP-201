c8fc2e7ef61c17a196164e7d21fc1a1ff4689039
Hokkaido <garook@e-mirim.hs.kr>
2023-05-25 11:00:39 +0900

    [SFML] 방향을 바꾸는 방식으로 전환

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -2,6 +2,13 @@
 #include <stdlib.h>		// srand(), rand()
 #include <time.h>		// time()
 
+#include <stdio.h>
+
+#define DIR_UP			0
+#define DIR_DOWN		1
+#define DIR_RIGHT		2
+#define DIR_LEFT		3
+
 using namespace sf;
 
 int main(void)
@@ -17,11 +24,12 @@ int main(void)
 	RenderWindow window(VideoMode(WIDTH, HEIGHT), "Snake Game");
 	// 1초에 60번의 작업이 이루어 지도록 frame 조절
 	// 컴퓨터 사양이 달라도 똑같은 속도로 처리함
-	window.setFramerateLimit(30);
+	window.setFramerateLimit(20);
 
 	srand(time(NULL));
 
 	RectangleShape snake;
+	int snake_dir = DIR_DOWN;
 	int snake_x = 3;
 	int snake_y = 3;
 	snake.setPosition(snake_x * block, snake_y * block);
@@ -48,24 +56,36 @@ int main(void)
 		// input
 		// 네 개의 방향키가 중복으로 input되면 안됨
 		if (Keyboard::isKeyPressed(Keyboard::Up)) {
-			snake_y--;
-			snake.move(0, -block);
+			snake_dir = DIR_UP;
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Down)) {
-			snake_y++;
-			snake.move(0, block);
+			snake_dir = DIR_DOWN;
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Right)) {
-			snake_x++;
-			snake.move(block, 0);
+			snake_dir = DIR_RIGHT;
 		}
 			
 		else if (Keyboard::isKeyPressed(Keyboard::Left)) {
-			snake_x--;
-			snake.move(-block, 0);
+			snake_dir = DIR_LEFT;
 		}
 			
 		// update
+		
+		if (snake_dir == DIR_UP) {
+			snake_y--;
+		}
+		else if (snake_dir == DIR_DOWN) {
+			snake_y++;
+		}
+		else if (snake_dir == DIR_RIGHT) {
+			snake_x++;
+		}
+		else if (snake_dir == DIR_LEFT) {
+			snake_x--;
+		}
+		snake.setPosition(snake_x * block, snake_y * block);
+		
+
 
 		// 뱀이 사과를 먹으면
 		if (snake.getGlobalBounds().intersects(apple.getGlobalBounds()))
